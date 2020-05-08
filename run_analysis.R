746f402d27f5469ee08f970e95febd026f259c9a
Federico Vincelli <vincellifederico@gmail.com>
2020-05-08 22:03:32 +0200

    Updated run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,38 +1,36 @@
-# Download the zip file if it does not exist already
+# Download the zip file if it does not exist already, then unzip it 
 uci_data <- "File.zip"
 if (!file.exists(uci_data)){
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(url, uci_data)}
-
-# Unizip the downloaded file
 if (!file.exists("UCI HAR Dataset")){
 unzip(uci_data)}
 
-
-# Read tables from the main folder 
+# Read the text files from the main folder and save it in the features and activities tables.
+# Descriptive names are given to the variables (as required by step 4 of the assignement). 
+# Then the process is repeated for the files contained in the test and train folders
 features <- read.table("./UCI HAR Dataset/features.txt", col.names = c("n", "functions"))
-activities <- read.table("./UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
-
-# Read file from the testing folder
+activities <- read.table("./UCI HAR Dataset/activity_labels.txt", col.names = c("id", "activity"))
 subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt", col.names = ("subject"))
-x_test <- read.table("./UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
 y_test <- read.table("./UCI HAR Dataset/test/y_test.txt", col.names = c("code"))
-
-# Read file from the training folder
 subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt", col.names = ("subject"))
-x_train <- read.table("./UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
 y_train <- read.table("./UCI HAR Dataset/train/y_train.txt", col.names = ("code"))
 
-# Merge tables
-x <- rbind(x_train, x_test)
-y <- rbind(y_train, y_test)
+# The values from the function columns in the features table are also used to name the columns
+# of these files
+x_test <- read.table("./UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+x_train <- read.table("./UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+
+# Merge tables obtained in the previous steps
+x <- rbind(x_train, x_test); y <- rbind(y_train, y_test)
 subject <- rbind(subject_train, subject_test)
 full_data <- cbind(subject, y, x)
 
-# Extract columns that contain the mean and the standard deviation of the measurements
+# Extract columns that contain the mean and the standard deviation of the measurements u
 tidy_data <- select(subject, code, contains("mean"), contains("std"))
 
-# Use descriptive names to name the activities in the data set
+# Use descriptive names to name the activities in the data set (the columns were already assigned
+# some descriptive names when read with read.tables)
 tidy_data$code <- activities[tidy_data$code, 2]
 
 # Rename using descriptive variable names
